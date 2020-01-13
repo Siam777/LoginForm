002d1a70a1a2cd900347d5e7b460d7507c045b0b
Siam777 <siamriaz065@gmail.com>
2020-01-14 03:10:10 +0600

    update

--- LoginRouting.js
+++ LoginRouting.js
@@ -14,18 +14,35 @@ app.config(function($routeProvider){
 		},
 		template:"Welcome User!"
 	})
+	.when('/signUp',{
+		templateUrl:'SignUp.html'
+	})
 	.otherwise({
 		redirectTo:'/'
 	});
 });
 app.controller('loginCtrl',function($scope,$location,$rootScope){
-	$scope.submit = function(){				
-		if($scope.UserName=="admin" && $scope.Password == 'admin'){
+	$scope.submit = function(){	
+	   var name= window.localStorage.getItem('name');
+	   var password = window.localStorage.getItem('password');
+	   console.log(name,password);			
+		if($scope.UserName==name && $scope.Password == password){
 			$rootScope.LoggedIn = true;
 			$location.path('/dashboard');
 		}
 		else{
 			alert("UserName or password not Matched")
 		}
+	}
+	$scope.signup=function(){
+		$location.path('/signUp');
+	}
+	$scope.RegisterForm = function(Register){
+	  	if(Register.Password==Register.ReconfirmPassword){
+	  		alert("Now you are Registered");
+	  		 window.localStorage.setItem('name',Register.UserName);
+	  		 window.localStorage.setItem('password',Register.Password);
+	  		$location.path("'/'");
+	  	}
 	}
 });
